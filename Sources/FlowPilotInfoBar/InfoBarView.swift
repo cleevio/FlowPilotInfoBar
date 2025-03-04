2635ba26b5d28cda212e34c335e369570e82fe77
Lukáš Valenta <valenta@lvalenta.cz>
2025-03-04 23:14:54 +0100

    Improved show + touch behavior

--- Sources/FlowPilotInfoBar/InfoBarView.swift
+++ Sources/FlowPilotInfoBar/InfoBarView.swift
@@ -29,17 +29,90 @@ public struct InfoBarView<T: View, InfoBarContent>: View {
             Color.clear
                 .allowsHitTesting(false)
 
-            if viewModel.isMessageShown {
-                view
-                    .padding(.top, viewModel.topPadding)
-                    .transition(transition)
-            }
-        }
-        .onAppear {
-            DispatchQueue.main.async {
-                viewModel.isMessageShown = true
-            }
+            content
         }
         .animation(animation, value: viewModel.isMessageShown)
+    }
+
+    @ViewBuilder
+    public var content: some View {
+        if #available(iOS 14.0, *) {
+            if #available(iOS 16.0, *) {
+                ViewThatFits(in: .vertical) {
+                    view
+                        .modifier(DragModifier(onDismiss: onDragDismiss))
+                    ScrollView {
+                        view
+                    }
+                    .modifier(DragModifier(onDismiss: onDragDismiss))
+                }
+            } else {
+                view
+                    .modifier(DragModifier(onDismiss: onDragDismiss))
+            }
+        } else {
+            view
+        }
+    }
+
+    func onDragDismiss() {
+        DispatchQueue.main.async {
+            viewModel.isMessageShown = false
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+struct DragModifier: ViewModifier {
+    var onDismiss: () -> Void
+
+    @State private var dragPopup = CGSize.zero
+    @GestureState private var isDragGestureActive: Bool = false
+    @State private var isDraggingActive: Bool = false
+
+    func body(content: Content) -> some View {
+        content
+            .offset(y: dragPopup.height)
+            .gesture(
+                DragGesture(minimumDistance: 30) // Set minimum distance, so small drags are ignored
+                    .onChanged { value in
+                        isDraggingActive = true
+                        withPreferredAnimation(.linear) {
+                            dragPopup = value.translation
+                        }
+                    }
+                    // Workaround: onEnded is not called
+                    // When some animation in the content view starts (like dragging slider),
+                    // `.onChange` can be called, but `.onEnded` is not called and the gesture is "cancelled" not "ended"
+                    // https://forums.developer.apple.com/forums/thread/660070
+                    .updating($isDragGestureActive) { _, state, _ in
+                        state = true
+                    }
+                    .onEnded { _ in
+                        isDraggingActive = false
+                    }
+            )
+            .onChange(of: isDragGestureActive) { isDragging in
+                isDraggingActive = isDragging
+            }
+            .onChange(of: isDraggingActive) { isDragging in
+                guard !isDragging else { return }
+                withPreferredAnimation(.linear) {
+                    if dragPopup.height < -50 {
+                        onDismiss()
+                    }
+                    dragPopup = .zero
+                }
+            }
+    }
+}
+
+@discardableResult
+@MainActor
+public func withPreferredAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+    if UIAccessibility.isReduceMotionEnabled {
+        return try body()
+    } else {
+        return try withAnimation(animation, body)
     }
 }
